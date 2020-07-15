fafa0b9340546bfd82d9a621723a59a88abd0aa1
Anthony Goossens <31324413+Creative-Yorkii@users.noreply.github.com>
2020-07-15 14:05:07 -0700

    Update styles.cc

---
+++ stylesheets/styles.cc
@@ -0,0 +1,83 @@
+img {
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  padding: 5px;
+  width: 150px;
+}
+
+body.black {
+  background-color: black;
+}
+
+body.orange {
+  background-color: orange;
+}
+
+body.grey {
+  background-color: grey;
+
+}
+h1.white {
+  color: white;
+}
+
+h2.font {
+  font-family: Catamaran;
+  color: white;
+}
+
+p.red {
+  background-color: red;
+}
+
+p.blue {
+  background-color: blue;
+}
+
+p.green {
+  background-color: green;
+}
+
+
+ul.b {
+  list-style-type: square;
+  color: white;
+}
+
+ol.d {
+  list-style-type: lower-alpha;
+}
+
+a {
+  outline: none;
+  text-decoration: none;
+  padding: 2px 1px 0;
+}
+
+a:link {
+  color: #265301;
+}
+
+a:visited {
+  color: #437A16;
+}
+
+a:focus {
+  border-bottom: 1px solid;
+  background: #BAE498;
+}
+
+a:hover {
+  border-bottom: 1px solid;
+  background: #CDFEAA;
+}
+
+a:active {
+  background: #265301;
+  color: #CDFEAA;
+}
+
+
+nav {
+  background-color: white;
+}
